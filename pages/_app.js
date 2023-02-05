76d0ec1f4049394d52c8b17adf52f084ff13ec05
Michael Corey Munn <coreymunn3@gmail.com>
2023-02-05 17:19:56 -0500

    setup react query

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
+import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../styles/theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export default function MyApp({ Component, pageProps }) {
+  const [queryClient] = React.useState(() => new QueryClient());
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools />
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
