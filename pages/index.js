7b32629b7f5991951b12521cf2ccf85cd94efeac
Michael Corey Munn <coreymunn3@gmail.com>
2023-02-05 20:12:39 -0500

    outlined basic skeleton for app

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
-import ColorModeSwitch from '@/components/ColorModeSwitch';
+import { Box } from '@chakra-ui/react';
+import Navbar from '@/components/Navbar';
+import NotesContainer from '@/components/NotesContainer';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
@@ -19,8 +21,25 @@ export default function Home() {
         <title>Simple Notes</title>
       </Head>
       <main>
-        <ColorModeSwitch />
-        <h1>Home Page</h1>
+        <Box mx={1}>
+          {/* Navbar */}
+          <Navbar />
+          {/* - Symbol & Title */}
+          {/* - Username (link to user page): TODO: Integrate Auth */}
+          {/* - Light/Dark Toggle */}
+
+          {/* Notes Container */}
+          <NotesContainer />
+
+          {/* - List of User's Notes with first one selected and Navbar Underneath (left) */}
+
+          {/* - - Ability to Add a Note */}
+          {/* - - Ability to Delete a Note */}
+
+          {/* - pane that shows the note contents (right) */}
+
+          {/* - - Ability to Edit contents (eventually, formatting the note contents) */}
+        </Box>
       </main>
     </>
   );
