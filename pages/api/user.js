c9658e39de8f7b8b575b1624596f2f4be2538084
Michael Corey Munn <coreymunn3@gmail.com>
2023-02-19 16:01:16 -0500

    improve error loggin in api user route

--- pages/api/user.js
+++ pages/api/user.js
@@ -7,7 +7,10 @@ export default async function handler(req, res) {
       const users = await prisma.user.findMany();
       return res.status(200).json(users);
     } catch (error) {
-      return res.status(500).json(error);
+      console.log(error);
+      return res.status(500).json({
+        message: 'Unable to find Users',
+      });
     }
   }
 }
