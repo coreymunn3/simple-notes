16786e58ce6a3ed6f7f792b63f8b1e1b80b964d0
Michael Corey Munn <coreymunn3@gmail.com>
2023-02-26 20:09:58 -0500

    Created update note mutation to set note content after debounce triggers

--- components/NotesContainer/components/UserNoteContent/index.js
+++ components/NotesContainer/components/UserNoteContent/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNote } from '@/contexts/NoteContext';
+import { useMutation } from '@tanstack/react-query';
 import { useEditor, EditorContent } from '@tiptap/react';
 import Underline from '@tiptap/extension-underline';
 import StarterKit from '@tiptap/starter-kit';
@@ -7,19 +8,35 @@ import Menubar from './components/Menubar';
 import ContentCard from '@/components/ContentCard';
 import { Box, Divider } from '@chakra-ui/react';
 import { debounce } from 'lodash';
+import axios from 'axios';
 
 const UserNoteContent = () => {
   const { activeNote } = useNote();
+
+  const updateNoteMutation = useMutation(
+    ['update-note'],
+    async (updatedContent) => {
+      const res = await axios.post('/api/note', {
+        ...activeNote,
+        content: updatedContent,
+      });
+      return res;
+    },
+    {
+      onSuccess: (data) => {},
+    }
+  );
 
   const editor = useEditor({
     extensions: [StarterKit, Underline],
     onUpdate: debounce(({ editor }) => {
       const json = editor.getJSON();
-      console.log(json);
-      // console.log('stringified', JSON.stringify(json));
-      // hit an endpoint to post or put a note, give it this content
+      const stringifiedContent = JSON.stringify(json);
+      console.log('stringifiedContent', stringifiedContent);
+      updateNoteMutation.mutate(stringifiedContent);
     }, 1000),
-    content: activeNote?.content || '',
+    // need to call setContent from editor commands whenever activeNote changes
+    content: '',
   });
 
   return (
